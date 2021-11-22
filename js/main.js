3471f4acf555e660ee1a48db19f92e25374eb709
Andreea1696 <andreeapetrescu16@gmail.com>
2021-11-22 15:14:01 +0100

    macro-step logici

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+
+TRACCIA: Attraverso l’apposita API di Boolean generare 10 indirizzi email
+e stamparli in pagina all’interno di una lista untilizzando Vue
+
+*/
+
+// collego l'elemento html in Vue 
+// creo proprietà per singola mail 
+// creo un array vuoto che sarà popolato con axios 
+// genero le 10 mail e le pusho nell'array 
